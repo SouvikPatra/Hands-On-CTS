0f8536ab7c9dc60cb9e4588c101becbc6df37173
Souvik <patrasouvik244@gmail.com>
2021-03-02 22:57:52 +0530

    commit changes

---
+++ Moq/MagicFilesLib/MagicFilesLib.UnitTest/DirectoryExplorerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace MagicFilesLib.UnitTest
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private Mock<IDirectoryExplorer> _directoryExplorer;
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _directoryExplorer = new Mock<IDirectoryExplorer>();
+            _directoryExplorer.Setup(x => x.GetFiles(It.IsAny<string>())).Returns(() => new List<string> { _file1, _file2 });
+        }
+
+        [Test]
+        [TestCase("hello world")]
+        [TestCase("welcome")]
+        public void GetFiles_Test(string input)
+        {
+            var files = _directoryExplorer.Object.GetFiles(input);
+
+            Assert.IsNotNull(files);
+            Assert.IsTrue(files.Count == 2);
+            Assert.That(files, Contains.Item(_file1));
+        }
+    }
+}
